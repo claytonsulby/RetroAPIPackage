385b407c52da3525cc049545c7847be73a25b1ab
Clayton Sulby <cls6275@psu.edu>
2021-04-11 21:07:19 -0400

    base request url fix

--- Sources/RetroAPIPackage/DoRequest.swift
+++ Sources/RetroAPIPackage/DoRequest.swift
@@ -44,7 +44,8 @@ public enum DoRequest {
         var components = URLComponents()
         components.scheme = "https"
         components.host = "retroachievements.org"
-        components.path = "dorequest.php" + "?r=" + request.rawValue
+        components.path = "dorequest.php"
+        components.queryItems?.append(contentsOf: [URLQueryItem(name: "r", value: request.rawValue)])
         
         return components
     }
