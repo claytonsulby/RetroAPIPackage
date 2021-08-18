65bcef604d71d50e1b7806d1c42a7bbe98a00b96
Clayton Sulby <cls6275@psu.edu>
2021-08-18 17:19:47 -0400

    added public initializer for gamiinfoanduserprogress

--- Sources/RetroAPIPackage/Data Transfer Objects/RetroAPI DTO/Games/GameInfoAndUserProgress_DTO.swift
+++ Sources/RetroAPIPackage/Data Transfer Objects/RetroAPI DTO/Games/GameInfoAndUserProgress_DTO.swift
@@ -7,6 +7,33 @@ import Foundation
 
 // MARK: - WelcomeElement
 public struct GameInfoAndUserProgress_DTO: Codable, Equatable {
+    public init(gameID_DTO: PHPHelper.PHPInt? = nil, title_DTO: String? = nil, consoleID_DTO: PHPHelper.PHPInt? = nil, forumTopicID_DTO: PHPHelper.PHPInt? = nil, flags_DTO: PHPHelper.PHPInt? = nil, imageIcon_DTO: String? = nil, imageTitle_DTO: String? = nil, imageInGame_DTO: String? = nil, imageBoxArt_DTO: String? = nil, publisher_DTO: String? = nil, developer_DTO: String? = nil, genre_DTO: String? = nil, released_DTO: String? = nil, isFinal_DTO: Bool? = nil, consoleName_DTO: String? = nil, richPresencePatch_DTO: String? = nil, numAchievements_DTO: PHPHelper.PHPInt? = nil, numDistinctPlayersCasual_DTO: PHPHelper.PHPInt? = nil, numDistinctPlayersHardcore_DTO: PHPHelper.PHPInt? = nil, achievements_DTO: GameInfoAndUserProgress_DTO.DictOrEmptyArray? = nil, numAwardedToUser_DTO: PHPHelper.PHPInt? = nil, numAwardedToUserHardcore_DTO: PHPHelper.PHPInt? = nil, userCompletion_DTO: String? = nil, userCompletionHardcore_DTO: String? = nil) {
+        self.gameID_DTO = gameID_DTO
+        self.title_DTO = title_DTO
+        self.consoleID_DTO = consoleID_DTO
+        self.forumTopicID_DTO = forumTopicID_DTO
+        self.flags_DTO = flags_DTO
+        self.imageIcon_DTO = imageIcon_DTO
+        self.imageTitle_DTO = imageTitle_DTO
+        self.imageInGame_DTO = imageInGame_DTO
+        self.imageBoxArt_DTO = imageBoxArt_DTO
+        self.publisher_DTO = publisher_DTO
+        self.developer_DTO = developer_DTO
+        self.genre_DTO = genre_DTO
+        self.released_DTO = released_DTO
+        self.isFinal_DTO = isFinal_DTO
+        self.consoleName_DTO = consoleName_DTO
+        self.richPresencePatch_DTO = richPresencePatch_DTO
+        self.numAchievements_DTO = numAchievements_DTO
+        self.numDistinctPlayersCasual_DTO = numDistinctPlayersCasual_DTO
+        self.numDistinctPlayersHardcore_DTO = numDistinctPlayersHardcore_DTO
+        self.achievements_DTO = achievements_DTO
+        self.numAwardedToUser_DTO = numAwardedToUser_DTO
+        self.numAwardedToUserHardcore_DTO = numAwardedToUserHardcore_DTO
+        self.userCompletion_DTO = userCompletion_DTO
+        self.userCompletionHardcore_DTO = userCompletionHardcore_DTO
+    }
+    
 
     private var gameID_DTO: PHPHelper.PHPInt?
     private var title_DTO: String?
