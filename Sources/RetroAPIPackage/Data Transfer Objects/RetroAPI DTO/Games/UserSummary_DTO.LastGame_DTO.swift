7e9b7e89269b72829e6c63e6de381cb646918e90
Clayton Sulby <cls6275@psu.edu>
2021-09-12 15:20:02 -0400

    added public initializer and equitable conformance for new LastGame_DTO struct

--- Sources/RetroAPIPackage/Data Transfer Objects/RetroAPI DTO/Games/UserSummary_DTO.LastGame_DTO.swift
+++ Sources/RetroAPIPackage/Data Transfer Objects/RetroAPI DTO/Games/UserSummary_DTO.LastGame_DTO.swift
@@ -9,10 +9,30 @@ import Foundation
 
 public extension UserSummary_DTO {
     
-    public struct LastGame_DTO: Codable {
+    public struct LastGame_DTO: Codable, Equatable {
+        public init(gameID: Int = 0, title: String = "", consoleID: Int = 0, forumTopicID: Int? = nil, flags: Int = 0, publisher: String? = nil, developer: String? = nil, genre: String? = nil, releasedDate: String? = nil, isFinal: Bool = false, consoleName: String = "", richPresencePatch: String = "", _imageIcon: String = "", _imageTitle: String = "", _imageInGame: String = "", _imageBoxArt: String = "") {
+            self.gameID = gameID
+            self.title = title
+            self.consoleID = consoleID
+            self.forumTopicID = forumTopicID
+            self.flags = flags
+            self.publisher = publisher
+            self.developer = developer
+            self.genre = genre
+            self.releasedDate = releasedDate
+            self.isFinal = isFinal
+            self.consoleName = consoleName
+            self.richPresencePatch = richPresencePatch
+            self._imageIcon = _imageIcon
+            self._imageTitle = _imageTitle
+            self._imageInGame = _imageInGame
+            self._imageBoxArt = _imageBoxArt
+        }
+        
         public var gameID: Int
         public var title: String
-        public var consoleID, forumTopicID, flags: Int
+        public var forumTopicID:Int?
+        public var consoleID, flags: Int
         public var publisher, developer, genre, releasedDate: String?
         public var isFinal: Bool
         public var consoleName, richPresencePatch: String
@@ -76,6 +96,25 @@ public extension UserSummary_DTO {
             case isFinal = "IsFinal"
             case consoleName = "ConsoleName"
             case richPresencePatch = "RichPresencePatch"
+        }
+        
+        public static func == (lhs: UserSummary_DTO.LastGame_DTO, rhs: UserSummary_DTO.LastGame_DTO) -> Bool {
+            return lhs.gameID == rhs.gameID &&
+                lhs.title == rhs.title &&
+                lhs.consoleID == rhs.consoleID &&
+                lhs.forumTopicID == rhs.forumTopicID &&
+                lhs.flags == rhs.flags &&
+                lhs.publisher == rhs.publisher &&
+                lhs.developer == rhs.developer &&
+                lhs.genre == rhs.genre &&
+                lhs.releasedDate == rhs.releasedDate &&
+                lhs.isFinal == rhs.isFinal &&
+                lhs.consoleName == rhs.consoleName &&
+                lhs.richPresencePatch == rhs.richPresencePatch &&
+                lhs._imageIcon == rhs._imageIcon &&
+                lhs._imageTitle == rhs._imageTitle &&
+                lhs._imageInGame == rhs._imageInGame &&
+                lhs._imageBoxArt == rhs._imageBoxArt
         }
     }
     
